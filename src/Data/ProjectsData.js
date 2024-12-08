32280a5a2d2a0794cbdaa2305372edd7aad878fa
Paulhenrisolat <paulhenrisolat@gmail.com>
2024-12-09 00:29:05 +0100

    Small adds & changes

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -4,27 +4,27 @@ const ProjectsData = [
         desc: "You are a little cube in a isometric 3D world, who protect the base with your friend.\n While infinite waves of blue jello zombie come's your way ! \n \n Made with Unity and Pun2 (Photon) i made a multiplayer hosting game with an ennemie wave system, \n who spawn ennemies, check if there all alive, and if not go to the next wave and spawn more."
     },
     {
-        name: "IsometricBuilder", img: ["isometricBuilder3.png","isometricBuilder2.png","isometricBuilder3.png","isometricBuilder4.png","isometricBuilder5.png"], minidesc: "Description for Project 4", tech: ["2D", "3D"], 
+        name: "IsometricBuilder", img: ["isometricBuilder3.png","isometricBuilder2.png","isometricBuilder3.png","isometricBuilder4.png","isometricBuilder5.png"], minidesc: "Template for isometric city builder", tech: ["C#", "Unity"], 
+        desc: "A Unity template to create isometric city builder game with 2D sprites."
+    },
+    {
+        name: "CrazySpace", img: ["crazySpace1.png","crazySpace2.png","crazySpace3.png","crazySpace4.png"], minidesc: "2D space shooter", tech: ["Lua","Love2D"], 
         desc: "description"
     },
     {
-        name: "CrazySpace", img: ["crazySpace1.png","crazySpace2.png","crazySpace3.png","crazySpace4.png"], minidesc: "Description for Project 2", tech: ["2D", "3D"], 
+        name: "BreakableGod", img: ["breakableGod1.png","breakableGod4.png","breakableGod2.png","breakableGod3.png"], minidesc: "Description for Project 3", tech: ["C#", "Love2D"], 
         desc: "description"
     },
     {
-        name: "BreakableGod", img: ["breakableGod1.png","breakableGod4.png","breakableGod2.png","breakableGod3.png"], minidesc: "Description for Project 3", tech: ["2D", "3D"], 
+        name: "BloodyBrickBreaker", img: ["bbb4.png","bbb3.png","bbb5.png","bbb6.png","bbb7.png","bbb2.png","bbb1.png"], minidesc: "Description for Project 4", tech: ["C++", "SFML"], 
         desc: "description"
     },
     {
-        name: "BloodyBrickBreaker", img: ["bbb4.png","bbb3.png","bbb5.png","bbb6.png","bbb7.png","bbb2.png","bbb1.png"], minidesc: "Description for Project 4", tech: ["2D", "3D"], 
+        name: "CreateEarth", img: ["testImg.png"], minidesc: "Description for Project 4", tech: ["C#", "Unity"], 
         desc: "description"
     },
     {
-        name: "CreateEarth", img: ["testImg.png"], minidesc: "Description for Project 4", tech: ["2D", "3D"], 
-        desc: "description"
-    },
-    {
-        name: "Gravity", img: ["testImg.png"], minidesc: "Description for Project 4", tech: ["2D", "3D"], 
+        name: "GravityDisaster", img: ["testImg.png"], minidesc: "Description for Project 4", tech: ["2D", "3D"], 
         desc: "description"
     },
 ]
--- src/Data/TechData.js
+++ src/Data/TechData.js
@@ -1,12 +1,18 @@
 const TechData = [
     {
-        name: "Unity",logo: "logoUnity.png",link: "https://unity.com"
+        name: "Unity",logo: "Unity-logo.png",link: "https://unity.com"
     },
     {
-        name: "C#",logo: "logoCsharp.png",link: "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
+        name: "C#",logo: "Csharp-logo.png",link: "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
     },
     {
         name: "Pun2",logo: "logoPun2.png",link: "https://www.photonengine.com/pun"
+    },
+    {
+        name: "Lua",logo: "Lua-Logo.png",link: "https://www.lua.org"
+    },
+    {
+        name: "Love2D",logo: "Love2d-Logo.png",link: "https://love2d.org"
     },
 ]
 
