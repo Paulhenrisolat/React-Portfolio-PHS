b8b58e0aa5d9c2c6f7ced9bb3955210a2d3f292c
Paulhenrisolat <psolat@gaming.tech>
2025-12-17 18:57:37 +0100

    NewCV

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,11 +3,11 @@ export default function Contact(){
         <div className="centerTxt">
             <div>
                 <h1>Contact</h1>
-                <a className="link" href="/React-Portfolio-PHS/CV-PHS-Prog.pdf" download="PaulHenriSolatCV.pdf">Click here to Download my CV !</a>
+                <a className="link" href="/React-Portfolio-PHS/CVPHS.pdf" download="PaulHenriSolatCV.pdf">Click here to Download my CV !</a>
             </div>
 
             <div>
-                <embed src="/React-Portfolio-PHS/CV-PHS-Prog.pdf" width="1500" height="1300" type="application/pdf"></embed>
+                <embed src="/React-Portfolio-PHS/CVPHS.pdf" width="1500" height="1300" type="application/pdf"></embed>
             </div>
             
         </div>
