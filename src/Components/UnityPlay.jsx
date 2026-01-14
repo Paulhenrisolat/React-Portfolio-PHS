9e3a9f300d713650e790860e2fa59ad25768270c
Paulhenrisolat <psolat@gaming.tech>
2026-01-14 16:47:30 +0100

    TestFixUnityWeb2

--- src/Components/UnityPlay.jsx
+++ src/Components/UnityPlay.jsx
@@ -1,15 +1,25 @@
+import React from "react";
+
 export default function UnityPlay({ unity }) {
+  // Build path relatif au dossier public
+  let path = unity?.buildPath || unity?.url || unity?.path || "";
+
+  // Gérer PUBLIC_URL pour GitHub Pages
+  const base = process.env.PUBLIC_URL || "";
+  const src = `${base}/${path}`.replace(/\/\/+/g, "/");
+
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <iframe
-        src={`${process.env.PUBLIC_URL}/${unity.url}`}
-        width={unity.width}
-        height={unity.height}
+        src={src}
+        width={unity.width || 960}
+        height={unity.height || 600}
         style={{
           border: "none",
           maxWidth: "100%",
         }}
         title="Unity WebGL"
+        allowFullScreen
       />
     </div>
   );
